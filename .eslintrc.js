85dee9406363dbd2ca62e0d5b2a71a3d8e4dd513
Yuriy Cherkashin <cherkashin.ju@gmail.com>
2022-06-23 17:07:29 +0300

    fix(lint): change eslint config
    Swap standard eslint config with airbnb config

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,24 +2,43 @@ module.exports = {
   root: true,
   env: {
     browser: true,
-    es2021: true
+    es2021: true,
+    node: true,
   },
   extends: [
+    'airbnb',
     'plugin:react/recommended',
-    'standard'
+    'plugin:react/jsx-runtime',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
-      jsx: true
+      jsx: true,
     },
     ecmaVersion: 'latest',
-    sourceType: 'module'
+    sourceType: 'module',
   },
   plugins: [
     'react',
-    '@typescript-eslint'
+    '@typescript-eslint',
   ],
   rules: {
-  }
-}
+    'react/jsx-filename-extension': ['error', { extensions: ['.tsx', '.jsx'] }],
+    'react/jsx-one-expression-per-line': 'off',
+  },
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      extends: ['airbnb-typescript'],
+      parserOptions: {
+        project: './tsconfig.json',
+      },
+    },
+    {
+      files: ['vite.config.ts'],
+      rules: {
+        'import/no-extraneous-dependencies': 'off',
+      },
+    },
+  ],
+};
