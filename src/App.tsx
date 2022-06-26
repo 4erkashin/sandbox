c58d2bb443d99a4d18409489d0f4b4d99a99ec1d
Yuriy Cherkashin <cherkashin.ju@gmail.com>
2022-06-26 20:40:37 +0300

    feat(iteration-1): add blog's first note

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,22 @@
+import CssBaseline from '@mui/material/CssBaseline';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+
 function App() {
   return (
-    <span>Very beginning</span>
+    <>
+      <CssBaseline />
+      <Container fixed>
+        <Typography variant="h1" gutterBottom>Very beginning</Typography>
+        {/* eslint-disable react/no-unescaped-entities */}
+        <Typography>So, even this 5 minute piece of shit could be called a working prototype of my
+          blog.
+          Yes, it's static. Yes, the content is only editable at the source code level. But this
+          content can already be created and distributed - that's the point I'm interested in all
+          this for.
+        </Typography>
+      </Container>
+    </>
   );
 }
 
