31c1b8e6b2a26fd146dc384e88cf0fbdc938c7d7
Yuriy Cherkashin <cherkashin.ju@gmail.com>
2022-07-18 21:13:17 +0300

    fix(fsd): move providers from entrypoint

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,29 +1,35 @@
+import React from 'react';
 import CssBaseline from '@mui/material/CssBaseline';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
+import { ThemeProvider } from '@mui/material/styles';
+
+import { theme } from './theme';
 
 export const App = () => (
-  <>
-    <CssBaseline />
-    <Container fixed>
-      <Typography variant="h1" gutterBottom>Very beginning</Typography>
-      <Typography gutterBottom>
-        So, even this 5 minute piece of shit could be called a working prototype of my blog.
-        Yes, it's static. Yes, the content is only editable at the source code level. But this
-        content can already be created and distributed - that's the point I'm interested in all
-        this for.
-      </Typography>
+  <React.StrictMode>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Container fixed>
+        <Typography variant="h1" gutterBottom>Very beginning</Typography>
+        <Typography gutterBottom>
+          So, even this 5 minute piece of shit could be called a working prototype of my blog.
+          Yes, it's static. Yes, the content is only editable at the source code level. But this
+          content can already be created and distributed - that's the point I'm interested in all
+          this for.
+        </Typography>
 
-      <Typography variant="h2" gutterBottom>Domain name</Typography>
-      <Typography gutterBottom>
-        First of all it's a string. String that using for application specific naming
-        and addressing purposes in various networking contexts within the Internet.
-      </Typography>
-      <Typography>
-        In general domain name identifies a network domain or an Internet Protocol resource (IP).
-        It's often used to identify services provided through the Internet such a websites,
-        emails etc.
-      </Typography>
-    </Container>
-  </>
+        <Typography variant="h2" gutterBottom>Domain name</Typography>
+        <Typography gutterBottom>
+          First of all it's a string. String that using for application specific naming
+          and addressing purposes in various networking contexts within the Internet.
+        </Typography>
+        <Typography>
+          In general domain name identifies a network domain or an Internet Protocol resource (IP).
+          It's often used to identify services provided through the Internet such a websites,
+          emails etc.
+        </Typography>
+      </Container>
+    </ThemeProvider>
+  </React.StrictMode>
 );
--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,6 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
-
-import CssBaseline from '@mui/material/CssBaseline';
-import { ThemeProvider } from '@mui/material/styles';
-
-import { App, theme } from './app';
+import { App } from './app';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <App />
-    </ThemeProvider>
-  </React.StrictMode>,
+  <App />,
 );
